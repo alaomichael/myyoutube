64a19a768f6f162b8fe7cc2e2c5b1f8b495d3b9d
alaomichael <contactleomax@gmail.com>
2020-09-09 05:11:01 +0100

    Updates

---
+++ precache-manifest.e4cfbfddded574d3b4abd0bdab7343fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3775344c8a3c0e3d48d5da5bfdf23dd",
+    "url": "/myyoutube/index.html"
+  },
+  {
+    "revision": "5dece5161f74aab45ba0",
+    "url": "/myyoutube/static/js/2.955b7035.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/myyoutube/static/js/2.955b7035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d0001984e408d4a418",
+    "url": "/myyoutube/static/js/main.1ef889ce.chunk.js"
+  },
+  {
+    "revision": "08cb6d94d340eb5edc08",
+    "url": "/myyoutube/static/js/runtime-main.00982245.js"
+  }
+]);
